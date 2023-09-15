bade8d6c3eb9703d867dc10e08f9fc9b34949229
Dhanwan <webexpertz.ntf@gmail.com>
2023-09-15 18:52:20 +0530

    Optimizing code 3

--- script/backup.sh
+++ script/backup.sh
@@ -6,13 +6,13 @@ source_file="/etc/wireguard/wg0.conf"
 backup_dir="/mnt/wg_backup"
 
 # Create a backup filename with the current date and time
-backup_filename="wg0_$(date +'%Y-%m-%d_%H-%M-%S').conf"
+backup_filename="wg0_$(/usr/bin/date +'%Y-%m-%d_%H-%M-%S').conf"
 
 # Check if a file with the same backup filename already exists
 if [ -e "$backup_dir/$backup_filename" ]; then
     echo "Backup file $backup_filename already exists. Skipping backup."
 else
     # Copy the source file to the backup directory with the new filename
-    cp "$source_file" "$backup_dir/$backup_filename"
+    /usr/bin/cp "$source_file" "$backup_dir/$backup_filename"
     echo "Backup of $source_file created as $backup_filename"
 fi
